08b9a67f4a7a302316296a6aca797b479f48160c
LSaldyt <lucassaldyt@gmail.com>
2024-02-06 12:59:33 -0700

    Add get_latest_experiment() function

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
 from .registry   import Registry
 from .experiment import Experiment
 from .settings   import Settings
+from .utilities  import get_latest_experiment
---
+++ utilities.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+import json
+
+from collections.abc import Mapping
+from typing import Tuple
+
+def get_latest_experiment(
+        experiment=None, experiment_path=None, 
+        parent_path='data/experiments/one_shot_plan') \
+        -> Tuple[Mapping, Path]:
+    parent_path = Path(parent_path)
+    if experiment is not None:
+        experiment_path = parent_path / experiment
+    if experiment_path is None:
+        experiment_path = list(parent_path.iterdir())[-1]
+
+    exp_metadata_path = experiment_path / 'meta.json'
+    exp_metadata = json.loads(exp_metadata_path.read_text())
+
+    exp_data = experiment_path / 'data'
+    return exp_metadata, exp_data
